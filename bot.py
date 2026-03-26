bd0ae2180ff0e0f94c5a1cf6ea71e0932ffde8fb
Pajan <d47708394@alumnos.itr.edu.ar>
2026-03-26 18:04:08 -0300

    god

---
+++ bot.py
@@ -0,0 +1,74 @@
+import discord
+import asyncio
+import datetime
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# --- configuracion ---
+TOKEN = os.getenv('DISCORD_TOKEN')
+CHANNEL_ID = int(os.getenv('CHANNEL_ID'))
+MUDAE_ID = 432610292342587392 
+# ---------------------
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'logeado como {self.user} ')
+        self.loop.create_task(self.cronometro_precision())
+
+    async def on_message(self, message):
+        if message.channel.id != CHANNEL_ID:
+            return
+
+        if message.author.id != MUDAE_ID:
+            return
+
+        contenido = message.content.lower()
+        
+        if "wished by" in contenido:
+            print(f'¡wishlist detectada! intentando click... (๑♡⌓♡๑)')
+            
+            if message.components:
+                for row in message.components:
+                    for component in row.children:
+                        if isinstance(component, discord.Button):
+                            try:
+                                await asyncio.sleep(1)
+                                
+                                await component.click()
+                                print('¡clic realizado!')
+                                return 
+                            except Exception as e:
+                                print(f'no pude hacer clic: {e}')
+
+    async def cronometro_precision(self):
+        while True:
+            ahora = datetime.datetime.now()
+            proximo = ahora.replace(minute=5, second=0, microsecond=0)
+            if ahora >= proximo:
+                proximo += datetime.timedelta(hours=1)
+            
+            espera = (proximo - ahora).total_seconds()
+            print(f'[reloj] esperando a las {proximo.strftime("%H:%M")}... (｡-ω-)zzZ')
+            
+            await asyncio.sleep(espera)
+            await self.enviar_tanda()
+
+    async def enviar_tanda(self):
+        channel = self.get_channel(CHANNEL_ID)
+        if channel:
+            print(f'lanzando tanda de las {datetime.datetime.now().strftime("%H:%M")}...')
+            for i in range(11):
+                try:
+                    await channel.send('$wa')
+                    print(f'   -> $wa {i+1}/10')
+                except Exception as e:
+                    print(f'error: {e}')
+                
+                # espera fija de 1 segundo entre cada $wa
+                await asyncio.sleep(1)
+            print('tanda terminada. volviendo a esperar... ')
+
+client = MyClient()
+client.run(TOKEN)
